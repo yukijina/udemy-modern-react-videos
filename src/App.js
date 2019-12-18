fe98203e8af3941b22960c514b62fd8c7dfe1c42
yukijina <yukijina@gmail.com>
2019-12-17 17:16:44 -0800

    imprt SearchBar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     render() {
 
         return (
-           <div><SearchBar /></div>
+           <div className="ui container"><SearchBar /></div>
         )
     }
 }
