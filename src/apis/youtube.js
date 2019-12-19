f0e3ef27aa41fa8100a8ee191deb2329aff5ea00
yukijina <yukijina@gmail.com>
2019-12-18 16:55:59 -0800

    add base URL and params

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -4,7 +4,7 @@ const KEY = process.env.REACT_APP_YOUTUBE_KEY;
 
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/search',
+    baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
