1716a9dca6a6c77c86e78505a3e19eb3ada08598
yukijina <yukijina@gmail.com>
2019-12-22 14:36:52 -0800

    add alt to the image tag

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,7 @@ import './VideoItem.css';
 const VideoItem = ({ video, onVideoSelect }) => {
     return (
         <div onClick={() => onVideoSelect(video)} className="video-item item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url}></img>
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url}></img>
             <div className="content">
                 <div className="header">
                     {video.snippet.title}
