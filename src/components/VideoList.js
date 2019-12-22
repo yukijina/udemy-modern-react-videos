9dacd20621d3d44f711bec1338ed50a6134d73c4
yukijina <yukijina@gmail.com>
2019-12-22 11:08:22 -0800

    add prop and pass it to the child(VideoItem)

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 //insted of passing 'props' we can add objet {videos}
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} />
     })
     
     return (
