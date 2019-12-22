a32daadd92682bf9e77629e324b9c6632e3aa513
yukijina <yukijina@gmail.com>
2019-12-22 14:37:18 -0800

    add key on VideoItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem';
 //insted of passing 'props' we can add objet {videos}
 const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem onVideoSelect={onVideoSelect} video={video} />
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />
     })
     
     return (
